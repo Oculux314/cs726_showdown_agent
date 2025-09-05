f17bfb72d7bd24b144e149139ef9a6e263ecdce1
Oculux314 <nate.williamson.nz@gmail.com>
2025-09-05 23:35:26 +1200

    adam w/ regen & hazards & improved max damage & forced switching based on types - #10

--- showdown_agent/scripts/players/nwil508.py
+++ showdown_agent/scripts/players/nwil508.py
@@ -1,3 +1,4 @@
+from typing import Any, Tuple, cast
 from poke_env.battle import AbstractBattle, Battle
 from poke_env.player import Player
 from poke_env import AccountConfiguration
@@ -5,7 +6,7 @@ from poke_env.player.battle_order import BattleOrder
 from poke_env.battle import Move
 from poke_env.battle import Pokemon
 from poke_env.calc.damage_calc_gen9 import calculate_damage
-from typing import Tuple, cast
+from poke_env.data.gen_data import GenData
 
 # MARK: TEAM
 team = """
@@ -73,8 +74,9 @@ Careful Nature
 """
 
 # MARK: GLOBALS
-round_count = 0
+gen9_data = GenData.from_gen(9)
 
+# MARK: INIT
 class CustomAgent(Player):
     def __init__(self, *args: AccountConfiguration | None, **kwargs: object):
         super().__init__(team=team, *args, **kwargs)
@@ -82,20 +84,27 @@ class CustomAgent(Player):
     def teampreview(self, battle: AbstractBattle) -> str:
         return "/team 123456"
 
-    # MARK: CHOOSE MOVE
     def choose_move(self, battle: AbstractBattle) -> BattleOrder:
         # Cast: Ensure battle is of correct type
         if not isinstance(battle, Battle):
             print("ERROR: Expected battle to be of type Battle")
             return self.choose_random_move(battle)
 
+        # Safeguard against unexpected errors
+        try:
+            if battle.force_switch:
+                return self.choose_switch(battle)
+            return self.choose_move_impl(battle)
+        except Exception as e:
+            print(f"ERROR: in main method: {e}")
+            return self.choose_max_damage_move(battle)
+
+    # MARK: CHOOSE MOVE
+    def choose_move_impl(self, battle: Battle) -> BattleOrder:
         # Is this ever possible?
         if (not battle.active_pokemon) or (not battle.opponent_active_pokemon):
             # Random for now
-            return self.choose_random_move(battle)
-
-        if battle.force_switch:
-            # TODO: Implement smart switch
+            print("WARN: Missing active pokemon, choosing random move")
             return self.choose_random_move(battle)
 
         ## If half health, and can regen, use it
@@ -127,6 +136,24 @@ class CustomAgent(Player):
                     return self.create_order(move)
 
         return self.choose_max_damage_move(battle)
+
+    # MARK: SWITCHING
+    def choose_switch(self, battle: Battle) -> BattleOrder:
+        # If we have no available switches, random
+        if not battle.opponent_active_pokemon:
+            print("INFO: No opponent active pokemon, choosing random move")
+            return self.choose_random_move(battle)
+
+        opposing_pokemon = battle.opponent_active_pokemon
+        # Find the best switch
+        best_switch = max(
+            battle.available_switches,
+            key=lambda poke: self.getTypeScoreTwoWay(poke, opposing_pokemon)
+        )
+
+        type_score = self.getTypeScoreTwoWay(best_switch, opposing_pokemon)
+        print(f"DEBUG: Switching to {best_switch.species} with type score {type_score:.2f}")
+        return self.create_order(best_switch)
 
     # MARK: DAMAGE HEURISTICS
     def estimate_damage(self, attacker: Pokemon, defender: Pokemon, move: Move, battle: Battle, attackingOpponent: bool) -> Tuple[float, float]:
@@ -182,6 +209,63 @@ class CustomAgent(Player):
         # This involves choosing a random move, which could be a switch or another available action
         print("WARN: No attacking move available. Choosing random move.")
         return self.choose_random_move(battle)
+
+    # MARK: TYPE HEURISTICS
+
+    # >0 means A is better, <0 means B is better
+    def getTypeScoreTwoWay(self, pokemonA: Pokemon, pokemonB: Pokemon) -> float:
+        scoreAToB = self.getTypeScoreOneWay(pokemonA, pokemonB) # A -> B
+        scoreBToA = self.getTypeScoreOneWay(pokemonB, pokemonA) # B -> A
+        if scoreBToA == 0:
+            print("WARN: Division by zero in type score calculation, returning large value")
+        return scoreAToB / scoreBToA
+
+    def getTypeScoreOneWay(self, attacker: Pokemon, defender: Pokemon) -> float:
+        score = 0.0
+        attacker_moves = self.getLearnableMoves(attacker)
+        attacker_type_histogram = self.getTypeHistogramForMoves(attacker_moves)
+
+        for attack_type, count in attacker_type_histogram.items():
+            if attack_type == "TOTAL":
+                continue
+            multiplier = 1.0
+            for defender_type in defender.types:
+                multiplier *= self.getTypeMultiplier(attack_type, defender_type.name)
+            # print(f"DEBUG: {attacker.species}'s {attack_type} moves have x{multiplier:.2f} effectiveness against {defender.species} (weight {count})")
+            score += multiplier * count
+
+        return score / attacker_type_histogram.get("TOTAL", 1)
+
+    def getTypeMultiplier(self, attacking_type: str, defending_type: str) -> float:
+        return gen9_data.type_chart.get(defending_type, {}).get(attacking_type, 1.0)
+
+    def getLearnableMoves(self, pokemon: Pokemon) -> dict[str, Any]:
+        learnset_of_original = gen9_data.learnset.get(pokemon.species)
+        learnset_of_base = gen9_data.learnset.get(pokemon.base_species) if pokemon.base_species != pokemon.species else {}
+        moveset = {**self.getLearnableMovesLearnset(learnset_of_base), **self.getLearnableMovesLearnset(learnset_of_original)}
+        if len(moveset) == 0:
+            print(f"ERROR: No species data for {pokemon.species}")
+            return {}
+        return moveset
+
+    def getLearnableMovesLearnset(self, learnset: Any) -> dict[str, Any]:
+        if not learnset:
+            return {}
+        move_ids = list(cast(dict[str, list[str]], learnset.get("learnset", {})).keys())
+        moveset = {move_name: gen9_data.moves[move_name] for move_name in move_ids if move_name in gen9_data.moves}
+        return moveset
+
+    def getTypeHistogramForMoves(self, moves: dict[str, Any]) -> dict[str, int]:
+        types = set(gen9_data.type_chart.keys())
+        type_histogram = {t: 0 for t in types} # Initialize all types to 0
+
+        for move in moves.values():
+            move_type = cast(str, move.get("type")).upper()
+            type_histogram[move_type] = type_histogram.get(move_type, 0) + 1
+            type_histogram["TOTAL"] = type_histogram.get("TOTAL", 0) + 1
+
+        # print(f"DEBUG: Move types histogram: {type_histogram}")
+        return type_histogram
 
     # MARK: HELPERS
 
